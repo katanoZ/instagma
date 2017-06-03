06f560e1ef581d14351dd938668029942bb187f7
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-06-04 01:55:10 +0900

    [update]ログイン時のみ画像一覧に遷移できるよう変更

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,7 @@
 class PicturesController < ApplicationController
 
   before_action :set_picture, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @pictures = Picture.all
