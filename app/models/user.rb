dedd883bd0b02bf9d1256ffcc4ded02c95c76ab5
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2017-06-05 23:37:57 +0900

    [add]サインアップ時のメール通知機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
+  has_many :pictures, dependent: :destroy  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :pictures, dependent: :destroy
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
